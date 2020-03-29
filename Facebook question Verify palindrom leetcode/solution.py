4632de4570d10da5a935bed9125fa7d17f162285
eliastam <elias.tamraz@mail.mcgill.ca>
2020-03-29 16:50:07 -0400

    palindrome

---
+++ Facebook question Verify palindrom leetcode/solution.py
@@ -0,0 +1,31 @@
+def isPalindrome(s):
+    n = len(s)
+    if n <2:
+        return True
+    i = 0 
+    j = n-1
+    while j >= i :
+        if not s[i].isalnum():
+            i+=1
+            continue
+        if not s[j].isalnum():
+            j-=1
+            continue
+        if s[i].lower() != s[j].lower():
+            return False
+        i +=1
+        j -=1
+    return True
+
+t = "..........b''ii887#%&Z8bba"
+k =0 
+l = list(t)
+while k < len(l):
+    if not l[k].isalnum():
+        l.remove(l[k])
+        if k >0:
+            k-=1
+        continue
+
+    k+=1
+print(l)
